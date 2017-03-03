54f2c69258e39a9a8f6f09b90440b4d35d8af7d6
Dwarven YANG <prison.yang@gmail.com>
2017-03-03 18:40:22 +0800

    init project

---
+++ AlamofireNetworkLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamofireNetworkLogger
+//
+//  Created by Dwarven on 2017/3/3.
+//  Copyright © 2017 Dwarven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
